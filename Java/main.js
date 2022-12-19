338bd2e3b42e329170aa748b56ead28260f7727b
Jean lucas <Lucas__jean@outlook.com>
2022-12-19 13:12:33 -0300

    new list projects

--- Java/main.js
+++ Java/main.js
@@ -6,7 +6,7 @@ var waypoint = new Waypoint({
     handler: function() {
       navigation.classList.toggle('display-flex')
     },
-    offset:'-10%'
+    offset:'50%'
   })
  
 //lista automatica das skills
@@ -24,3 +24,39 @@ setInterval( function () {
   }
   document.getElementById("slide"+count).checked = true;
  }
+
+
+ //waypoint da lista de projetos
+
+let p1 = document.querySelector('#p1')
+var waypoint = new Waypoint({
+    element: document.querySelector('#p1'),
+    handler: function() {
+      p1.classList.toggle('opacity-right')
+    },
+    offset:'50%'
+})
+let p2 = document.querySelector('#p2')
+var waypoint = new Waypoint({
+    element: document.querySelector('#p2'),
+    handler: function() {
+      p2.classList.toggle('opacity-left')
+    },
+    offset:'50%'
+})
+let p3 = document.querySelector('#p3')
+var waypoint = new Waypoint({
+    element: document.querySelector('#p3'),
+    handler: function() {
+      p3.classList.toggle('opacity-right')
+    },
+    offset:'50%'
+})
+let p4 = document.querySelector('#p4')
+var waypoint = new Waypoint({
+    element: document.querySelector('#p4'),
+    handler: function() {
+      p4.classList.toggle('opacity-left')
+    },
+    offset:'50%'
+})
